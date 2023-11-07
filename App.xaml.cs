f9235610d76d3629bb55b92fffac5ab57e9d8a4e
McAr0v <dvijkz@gmail.com>
2023-11-07 17:26:51 +0600

    Добавьте файлы проекта.

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace DvijCShapr;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace DvijCShapr;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
