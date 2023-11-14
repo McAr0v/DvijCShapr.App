9fd64db66081d26d1d414358e43229ccc8257971
McAr0v <dvijkz@gmail.com>
2023-11-14 21:49:10 +0600

    Получилось реализовать меню

--- AppShell.xaml.cs
+++ AppShell.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using DvijCShapr.Pages;
 using DvijCShapr.Pages.Events;
 using DvijCShapr.Pages.Other;
+using DvijCShapr.Pages.Profile;
 
 namespace DvijCShapr;
 
@@ -15,11 +16,21 @@ public partial class AppShell : Shell
         Routing.RegisterRoute("AboutAdPage", typeof(AboutAdPage));
         Routing.RegisterRoute("FeedbackPage", typeof(FeedbackPage));
         Routing.RegisterRoute("PrivacyPolicyPage", typeof(PrivacyPolicyPage));
+        Routing.RegisterRoute("ProfilePage", typeof(ProfilePage));
+        Routing.RegisterRoute("AboutAppPage", typeof(AboutAppPage));
 
         
     }
 
-    
+    private async void ToolbarItem_Clicked(object sender, EventArgs e)
+    {
+        await Shell.Current.GoToAsync("ProfilePage");
+    }
 
-        
+    private async void MenuItem_Clicked(object sender, EventArgs e)
+    {
+        Shell.Current.FlyoutIsPresented = false;
+        await Shell.Current.GoToAsync("AboutAppPage");
+               
+    }
 }
